13c20b677d87390e2e0ead02f72af19c1c536c34
Miguel Sanchez <mangelsr@live.com>
2017-08-10 16:22:49 -0500

    [ADD] Funcion de mostrar parametros

--- pc.c
+++ pc.c
@@ -1,26 +1,42 @@
 #include <stdio.h>
 #include <pthread.h>
-#include <cstdlib>
+#include <stdlib.h>
 
 pthread_mutex_t mutex;
 pthread_cond_t condicionConsumidor;
 pthread_cond_t condicionProductor;
 
+int tam_cola = 0;
+
+void mostrarinfo(int nhilos_prod, int nhilos_cons, double tiempo_cons, double tiempo_prod, int total_items);
+
 int main(int argc, char** argv)
 {
   if (argc!=7){
-  	printf("Uso del programa <num_hilos_prod> <tiempo_prod> <num_hilos_cons> <tiempo_cons> <tam_cola>
-		<total_items>\n");
+  	printf("Uso del programa <num_hilos_prod> <tiempo_prod> <num_hilos_cons> <tiempo_cons> <tam_cola> <total_items>\n");
   	return -1;
   }
   
   int nhilos_prod = atoi(argv[1]);
-  int tiempo_prod = atof(argv[2]);
+  double tiempo_prod = atof(argv[2]);
   int nhilos_cons = atoi(argv[3]);
-  int nhilos_cons = atof(argv[4]);
-  int tam_cola = atoi(argv[5]);
+  double tiempo_cons = atof(argv[4]);
+  tam_cola = atoi(argv[5]);
   int total_items = atoi(argv[6]);
+
+  mostrarinfo(nhilos_prod, nhilos_cons, tiempo_cons, tiempo_prod, total_items);
+
+  condicionConsumidor = PTHREAD_COND_INITIALIZER;
+  condicionProductor = PTHREAD_COND_INITIALIZER;
 
 
   return 0;
+}
+
+void mostrarinfo(int nhilos_prod, int nhilos_cons, double tiempo_cons, double tiempo_prod, int total_items){
+	printf("Número de productores: %d\n", nhilos_prod);
+	printf("Número de consumidores: %d\n", nhilos_cons);
+	printf("Tiempo de consumo: %.2f segundos", tiempo_cons);
+	printf("Tiempo de producción: %.2f segundos", tiempo_prod);
+	printf("Total de ítems a producir: %d elementos.\n", total_items);
 }
